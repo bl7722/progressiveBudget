004dfd48601909778dca1396f44a7dc5d8322d78
Brandon Lopez <brandon.i.lopez77@gmail.com>
2020-07-02 00:11:48 -0500

    updated server.js. put var MONGODB_URI seperate from userNewUrlParser, etc. Think this time should work

--- server.js
+++ server.js
@@ -15,11 +15,18 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-var MONGODB_URI = (process.env.MONGODB_URI || "mongodb://user1:password1@ds239412.mlab.com:39412/heroku_555wf2d2", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+
+var MONGODB_URI= process.env.MONGODB_URI || "mongodb://user1:password1@ds239412.mlab.com:39412/heroku_555wf2d2"
+
 mongoose.connect(MONGODB_URI);
+
+mongoose.connect("mongodb://localhost/budget",{
+useNewUrlParser: true,
+useFindAndModify: false
+});
+
+
+
 // routes
 app.use(require("./routes/api.js"));
 
